0762cf51d64686215743b461cb5ca98568c4620c
aaerok <bbk001@ucsd.edu>
2019-06-08 14:59:00 -0700

    final commit

--- public/js/recipe.js
+++ public/js/recipe.js
@@ -1,13 +1,9 @@
-$(document).ready(() => {
-    // $(document).on('mouseenter', '#recipe-pic', () => {
-    //     $('.recipe-button').show();
-    // }).on('mouseleave', '#recipe-pic', () => {
-    //     $('.recipe-button').hide();
-    // });
+/* recipe.js handles the client-side js for the recipe page. It handles things
+such as the handlebars template for when users want to search for a recipe.
+It also handles the recipe search box directly in the recipe page itself through
+an ajax request. */
 
-    var source = $("#recipe-query").html();
-    var template = Handlebars.compile(source);
-    var parentDiv = $("#query-list");
+$(document).ready(() => {
     const urlParams = new URLSearchParams(window.location.search);
     if (urlParams) {
         $('#recipe-search-box').val(urlParams.get('search'));
@@ -17,8 +13,6 @@ $(document).ready(() => {
     $('#recipe-search-button').click(() => {
         if ($('#recipe-search-box').val().length !== 0) {
             $('#query-list').empty();
-            $('.all-recipes').hide();
-            // const search = '/recipe/search/' + $('#recipe-search-box').val();
             const search = '/allRecipes';
             const string = $('#recipe-search-box').val();
             $.ajax({
@@ -27,20 +21,30 @@ $(document).ready(() => {
                 dataType: 'json',
                 success: (data) => {
                     for (var i = 0; i < data.length; i++) {
-                        if (data[i].name.toLowerCase().includes(string.toLowerCase())) {
-                            var html = template(data[i]);
-                            parentDiv.append(html);
+                        if (!(data[i].name.toLowerCase().includes(string.toLowerCase()))) {
+                            $('.recipe' + i).hide();
+                        } else {
+                            $('.recipe' + i).show();
                         }
                     }
                 }
             });
-        }   
+        }
     });
 
     $('#recipe-search-box').keyup(() => {
         if ($('#recipe-search-box').val().length === 0) {
-            $('#query-list').empty();
-            $('.all-recipes').show();
+            const search = '/allRecipes';
+            $.ajax({
+                url: search,
+                type: 'GET',
+                dataType: 'json',
+                success: (data) => {
+                    for (var i = 0; i < data.length; i++) {
+                        $('.recipe' + i).show();
+                    }
+                }
+            });
         }
     });
 
@@ -53,4 +57,12 @@ function show(i) {
 
 function hide(i) {
     $('.show-single-recipe' + i).addClass('recipe-hidden');
+}
+
+function showQuery(i) {
+    $('.show-single-query' + i).removeClass('recipe-hidden');
+}
+
+function hideQuery(i) {
+    $('.show-single-query' + i).addClass('recipe-hidden');
 }
